f3f2f1a182801e9195ea9fbe6e225cef029cd470
vnightly <hobbydoc@bk.ru>
2024-11-13 12:09:35 +0300

    testone

--- src/test/java/org/example/TestOne.java
+++ src/test/java/org/example/TestOne.java
@@ -1,5 +1,16 @@
 package org.example;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
 
 public class TestOne {
+    @Test
+    public void testone(){
+        int x = 2;
+        Assert.assertTrue(x >= 0);
+    }
+
+
 
 }
